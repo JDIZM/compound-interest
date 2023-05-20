eabb1a79123d83b6790dcd90fa07cfbb0a6b7c24
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2023-05-20 17:13:28 +0100

    feat: test package with js/ts

---
+++ test-package/index.ts
@@ -0,0 +1,5 @@
+// import { compoundInterest } from "compound-interest/dist/calc/compoundInterest.js";
+import { compoundInterest } from "compound-interest";
+
+const test = compoundInterest(100, 0.1, 1);
+console.log(test);
