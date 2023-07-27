6e240b34a0e4e82883e03c474e30a822dff15639
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2023-07-28 00:06:20 +0100

    chore: commit dist

--- dist/calc/compoundInterest.js
+++ dist/calc/compoundInterest.js
@@ -1,36 +1,128 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.calcInvestmentWithInterest = exports.compoundInterest = void 0;
-const compoundInterest = (principal, rate, time, paymentFrequency) => {
-    console.log("calculating compound interest..");
-    console.log("principal: ", principal);
-    console.log("rate: ", rate);
-    console.log("time: ", time);
-    console.log("paymentFrequency: ", paymentFrequency);
+exports.calcMortgage = exports.compoundInterestPerPeriod = exports.calcTotalPayments = exports.calcInvestmentWithInterest = exports.compoundInterestOverYears = void 0;
+const compoundInterestOverYears = (principal, rate, years) => {
     if (rate >= 1) {
         rate = rate / 100;
     }
-    console.log("rate: ", rate);
-    const totalPayments = time * paymentFrequency;
-    console.log("totalPayments: ", totalPayments);
-    const ratePerPeriod = rate / paymentFrequency;
-    console.log("ratePerPeriod: ", ratePerPeriod);
-    const multiplier = Math.pow(1 + ratePerPeriod, time * paymentFrequency);
-    console.log("pow: ", multiplier);
+    const multiplier = Math.pow(1 + rate, years);
     return principal * multiplier;
 };
-exports.compoundInterest = compoundInterest;
-const calcInvestmentWithInterest = (principal, rate, time, paymentFrequency, amountPerAnnum) => {
-    const balance = (0, exports.compoundInterest)(principal, rate, time, paymentFrequency);
-    const totalInvestment = principal + amountPerAnnum * time;
-    const totalPayments = time * paymentFrequency;
+exports.compoundInterestOverYears = compoundInterestOverYears;
+const calcInvestmentWithInterest = (principal, rate, years, paymentsPerAnnum, amountPerAnnum) => {
+    const finalBalance = (0, exports.compoundInterestOverYears)(principal, rate, years);
+    const totalInvestment = principal + amountPerAnnum * years;
+    const totalPayments = years * paymentsPerAnnum;
     return {
         principal,
-        balance,
+        finalBalance,
         totalPayments,
         totalInvestment,
-        totalInterest: balance - totalInvestment
+        totalInterest: finalBalance - totalInvestment
     };
 };
 exports.calcInvestmentWithInterest = calcInvestmentWithInterest;
+const calcTotalPayments = (years, paymentsPerAnnum, type) => {
+    switch (type) {
+        case "lumpSum":
+            return 1;
+        case "contribution":
+        case "debtRepayment":
+        default:
+            return years * paymentsPerAnnum;
+    }
+};
+exports.calcTotalPayments = calcTotalPayments;
+const compoundInterestPerPeriod = (options) => {
+    let { rate } = options;
+    const { principal, years, paymentsPerAnnum = 1, amountPerAnnum = 0, accrualOfPaymentsPerAnnum = false, currentPositionInYears, debtRepayment = false } = options;
+    if (rate >= 1) {
+        rate = rate / 100;
+    }
+    let investmentType = "lumpSum";
+    if (!accrualOfPaymentsPerAnnum) {
+        if (amountPerAnnum > 0) {
+            investmentType = "contribution";
+        }
+        if (debtRepayment) {
+            investmentType = "debtRepayment";
+        }
+    }
+    const totalPayments = (0, exports.calcTotalPayments)(years, paymentsPerAnnum, investmentType);
+    const ratePerPeriod = rate / paymentsPerAnnum;
+    const multiplierTotal = Math.pow(1 + rate, years);
+    const multiplierPerPeriod = 1 + ratePerPeriod;
+    let totalInvestment = accrualOfPaymentsPerAnnum ? principal + amountPerAnnum * years : principal + amountPerAnnum;
+    if (investmentType === "contribution") {
+        totalInvestment = principal + amountPerAnnum * years;
+    }
+    if (debtRepayment && !accrualOfPaymentsPerAnnum) {
+        totalInvestment = amountPerAnnum * years;
+    }
+    const interestPerAnnum = [];
+    const interestMatrix = new Map();
+    let prevBalance = principal;
+    let currentBalance = principal;
+    for (let i = 0; i < years; i++) {
+        const monthlyBalance = [];
+        if (i > 0) {
+            prevBalance = interestMatrix.get(`${i}`)[paymentsPerAnnum - 1];
+        }
+        const interestThisYear = prevBalance * rate;
+        const interestParts = [];
+        if (!accrualOfPaymentsPerAnnum)
+            interestPerAnnum.push(interestThisYear);
+        for (let p = 0; p < paymentsPerAnnum; p++) {
+            if (accrualOfPaymentsPerAnnum) {
+                const newBalanceWithAccrual = prevBalance + amountPerAnnum / paymentsPerAnnum;
+                const interest = newBalanceWithAccrual * ratePerPeriod;
+                prevBalance = prevBalance + interest + amountPerAnnum / paymentsPerAnnum;
+                interestParts.push(interest);
+            }
+            else {
+                const interest = interestThisYear / paymentsPerAnnum;
+                prevBalance = prevBalance + interest;
+            }
+            monthlyBalance.push(Number(prevBalance.toFixed(2)));
+        }
+        if (accrualOfPaymentsPerAnnum) {
+            const totalInterestForYear = interestParts.reduce((a, b) => a + b, 0);
+            interestPerAnnum.push(totalInterestForYear);
+        }
+        interestMatrix.set(`${i + 1}`, monthlyBalance);
+    }
+    if (currentPositionInYears) {
+        currentBalance = interestMatrix.get(`${currentPositionInYears}`)[paymentsPerAnnum - 1];
+    }
+    else {
+        currentBalance = interestMatrix.get(`${years}`)[paymentsPerAnnum - 1];
+    }
+    const totalInterest = interestPerAnnum.reduce((a, b) => a + b, 0);
+    const endBalance = accrualOfPaymentsPerAnnum
+        ? interestMatrix.get(`${years}`)[paymentsPerAnnum - 1]
+        : principal * multiplierTotal;
+    return {
+        principal,
+        rate,
+        years,
+        currentPositionInYears,
+        paymentsPerAnnum,
+        totalPayments,
+        ratePerPeriod,
+        multiplierTotal,
+        multiplierPerPeriod,
+        totalInvestment,
+        interestMatrix,
+        interestPerAnnum,
+        currentBalance,
+        totalInterest,
+        endBalance,
+        accrualOfPaymentsPerAnnum,
+        investmentType
+    };
+};
+exports.compoundInterestPerPeriod = compoundInterestPerPeriod;
+const calcMortgage = (options) => {
+};
+exports.calcMortgage = calcMortgage;
 //# sourceMappingURL=compoundInterest.js.map
