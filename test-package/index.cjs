76d9ac8fbde097384c3a587f0db9a50cdf0598e4
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2024-01-28 10:01:12 +0000

    chore: test package examples

---
+++ test-package/index.cjs
@@ -0,0 +1,15 @@
+const { compoundInterestPerPeriod } = require("compound-interest")
+
+// example interest only payment
+const valueOfHome = compoundInterestPerPeriod({
+  principal: 150_000,
+  rate: 4,
+  years: 5,
+  paymentsPerAnnum: 12,
+  amountPerAnnum: 12_000,
+  debtRepayment: {
+    interestRate: 6,
+    type: "interestOnly"
+  }
+});
+console.log("valueOfHome", valueOfHome);
